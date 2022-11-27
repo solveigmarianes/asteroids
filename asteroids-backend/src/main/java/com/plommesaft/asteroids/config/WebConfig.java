ff1916de61a59465177ae07563f71293dd640bdc
Solveig Maria Nes <solveigmaria.nes@greenbird.com>
2022-11-27 20:41:15 +0100

    Add CORS registry mapping

--- asteroids-backend/src/main/java/com/plommesaft/asteroids/config/WebConfig.java
+++ asteroids-backend/src/main/java/com/plommesaft/asteroids/config/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**").allowedMethods("*");
+        corsRegistry.addMapping("/api/asteroids*").allowedMethods("*");
     }
 }
